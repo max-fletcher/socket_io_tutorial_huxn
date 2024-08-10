cc742e74261fdb72a9db25648fc9a988a7fe82d5
Mahin Chowdhury <54007446+max-fletcher@users.noreply.github.com>
2024-08-10 23:10:18 +0600

    Socket working but architecture needs to change.

--- chat-app/client/src/App.jsx
+++ chat-app/client/src/App.jsx
@@ -1,51 +1,89 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import './App.css'
-import { socketServer } from './utils/socket'
+import { io } from 'socket.io-client';
+// import { socketServer } from './utils/socket'
 
 function App() {
+  let socketServer = useRef(null); // Socket Ref
+  const socketUrl = process.env.NODE_ENV === 'production' ? undefined : 'http://localhost:4001';
+
   // Establish socket connection
-  const [isConnected, setIsConnected] = useState(socketServer.connected)
+  const [isConnected, setIsConnected] = useState(false)
   const [socketEvents, setSocketEvents] = useState([])
+  const [userId, setUserId] = useState('')
+  const [userName, setUserName] = useState('')
+  const [userData, setUserData] = useState({ id: '', name: '' })
 
   useEffect(() => {
-    function onConnect() {
-      setIsConnected(true);
-    }
+    if(isConnected){
+      console.log('connecting...')
+      if(!userId.length && userName.length){
+        setIsConnected(false)
+        return
+      }
 
-    function onDisconnect() {
-      setIsConnected(false);
-    }
+      setUserData({ id: userId, name: userName })
+      socketServer.current = io(socketUrl, {auth: { user: userData }})
 
-    function onSocketEvent(value) {
-      setSocketEvents(previous => [...previous, value]);
-    }
+      function onSocketEvent(value) {
+        setSocketEvents(previous => [...previous, value]);
+      }
 
-    socketServer.on('connect', onConnect)
-    socketServer.on('disconnect', onDisconnect)
-    socketServer.on('socketEvent', onSocketEvent)
+      socketServer.current.on('connect', console.log("Connected to server"))
+      socketServer.current.on('disconnect', console.log("Disconnected from server"))
+      socketServer.current.on('socketEvent', onSocketEvent)
+
+      socketServer.current.connect()
+      setUserId('')
+      setUserName('')
+    }
+    else{
+      console.log('disconnecting...')
+      if(socketServer.current){
+        socketServer.current.disconnect()
+        setUserId('')
+        setUserName('')
+      }
+    }
 
     return () => {
-      socketServer.off('connect', onConnect)
-      socketServer.off('disconnect', onDisconnect)
-      socketServer.off('socketEvent', onSocketEvent);
+      if(socketServer.current){
+        socketServer.current.off('connect')
+        socketServer.current.off('disconnect')
+        socketServer.current.off('socketEvent')
+        socketServer.current = null
+      }
     }
-  }, [])
+  }, [socketUrl, userId, userName, userData, isConnected])
 
   function connect() {
-    socketServer.connect();
+    setIsConnected(true)
   }
 
   function disconnect() {
-    socketServer.disconnect();
+    setIsConnected(false)
   }
 
-  // const [connect, setConnect] = useState(false)
-  // const [userId, setUserId] = useState('')
-  // const [userName, setUserName] = useState('')
-  // const [userData, setUserData] = useState({ id: '', name: '' })
+  const [roomName, setRoomName] = useState('')
+  const enterRoom = () => {
+    if(roomName === '')
+      alert('Room name is empty')
 
-  // const [roomName, setRoomName] = useState('')
-  // const [message, setMessage] = useState('')
+    socketServer.current.emit("join room", roomName, userData)
+    console.log("joined room", roomName, userData)
+    setRoomName('')
+  }
+
+
+  const [message, setMessage] = useState('')
+  const sendMessage = () => {
+    if(message === '')
+      alert('Message is empty')
+
+    socketServer.current.emit("send message", roomName, userData, message)
+    console.log("sent message", roomName, userData, message)
+    setMessage('')
+  }
 
   return (
     <>
@@ -60,16 +98,21 @@ function App() {
           )
         }
       </ul>
-
+      <input type="text" onChange={(e) => {setUserId(e.target.value)}} value={userId} placeholder='Enter User Id' />
+      <input type="text" onChange={(e) => {setUserName(e.target.value)}} value={userName} placeholder='Enter User Name' />
+      <br />
       <button onClick={ connect }>Connect</button>
       <button onClick={ disconnect }>Disconnect</button>
+      <br />
 
-      {/* <input type="text" onChange={(e) => {setUserId(e.target.value)}} value={userId} placeholder='Enter User Id' />
-      <input type="text" onChange={(e) => {setUserName(e.target.value)}} value={userName} placeholder='Enter User Name' />
+      <input type="text" onChange={(e) => {setRoomName(e.target.value)}} value={roomName} placeholder='Enter Room Name.' />
+      <br />
+      <button onClick={ enterRoom } disabled={isConnected}>Enter room</button>
+      <br />
 
-      <input type="text" onChange={(e) => {setUserName(e.target.value)}} value={userName} placeholder='Enter User Name' />
-
-      <input type="text" onChange={(e) => {setUserName(e.target.value)}} value={userName} placeholder='Enter User Name' /> */}
+      <input type="text" onChange={(e) => {setMessage(e.target.value)}} value={message} placeholder='Enter Message' />
+      <br />
+      <button onClick={ sendMessage } disabled={isConnected}>Send Message</button>
     </>
   )
 }
